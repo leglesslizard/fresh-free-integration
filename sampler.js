5fa2c15c2f6f627fc2d353f60abce66f6d5b4658
Joey <jpg@neilpie.co.uk>
2016-03-23 18:42:19 +0000

    project and task list now in place, need to be loaded when html elements become available

--- sampler.js
+++ sampler.js
@@ -1,4 +1,5 @@
 var freeagent_token = '';
+var freeagent_url   = 'https://api.sandbox.freeagent.com/v2/';
 // Refresh access token on page load but don't do anything else until the request has been processed
 // If an access token is found then functionality will be added to the page in the "LoadItUp" method
 refreshToken( '3ZmVwTjVPKmdHDmBhpq-uA', 'yF4e7ajdL0M5YPv-Eh3eTg', '1rZkfkZxrhIlWFHrJ7K_P95LBIXzlYJ0rYORQ5ySo' );
@@ -9,7 +10,7 @@ refreshToken( '3ZmVwTjVPKmdHDmBhpq-uA', 'yF4e7ajdL0M5YPv-Eh3eTg', '1rZkfkZxrhIlW
  * @returns {XMLHttpRequest}
  */
 function refreshToken( client_id, client_secret, refresh_token) {
-    var xhr = doRequest( 'POST','https://api.sandbox.freeagent.com/v2/token_endpoint' );
+    var xhr = doRequest( 'POST','token_endpoint' );
     xhr.setRequestHeader( 'Authorization', 'Basic ' + client_id + ':' + client_secret );
     xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
     xhr.onload = receiveNewToken;
@@ -25,7 +26,7 @@ function refreshToken( client_id, client_secret, refresh_token) {
  */
 function doRequest( method, url ) {
     var xhr = new XMLHttpRequest();
-    xhr.open( method, url, true );
+    xhr.open( method, freeagent_url+url, true );
     return xhr;
 }
 
@@ -60,14 +61,120 @@ function loadItUp( access_token ) {
  * Hook into the xhr for adding time and create freeagent timeslip
  */
 function catchXhr() {
-    jQuery(document).ajaxSend(function(e, xhr, settings) {
-        if ( '/helpdesk/tickets/390/time_sheets' == settings.url && 'undefined' !== typeof settings.data ) {
-            if ( settings.data.indexOf( 'time_entry' ) > 5 ) {
-                addFreeAgentTimeslip( settings.data );
+
+    // Mock up for local @todo Remove when working on freshdesk
+    if ( 'local.wordpress.dev' === window.location.hostname ) {
+        data = 'utf8=%E2%9C%93&authenticity_token=WhAhKozM8HkolyO3Qa%2BX6cWHBFQqIMjm0cgHVwg%2FiRQ%3D&time_entry%5Bworkable_id%5D=9005487861&time_entry%5Buser_id%5D=9006138143&time_entry%5Bhhmm%5D=01%3A10&time_entry%5Bbillable%5D=0&time_entry%5Bexecuted_at%5D=23+Mar%2C+2016&time_entry%5Bnote%5D=njhgfgjhkb%2Cn';
+        displayFreeAgentProjects();
+        displayFreeAgentTasks();
+        addFreeAgentTimeslip( data );
+    } else {
+
+        // @todo add dropdown for freeagent projects
+        // @todo add dropdown for hour types - developer/senior
+        jQuery( document ).ajaxSend( function ( e, xhr, settings ) {
+            if ( '/helpdesk/tickets/390/time_sheets' == settings.url ) {
+                if ( 'undefined' !== typeof settings.data && settings.data.indexOf( 'time_entry' ) > 5 ) {
+                    addFreeAgentTimeslip( settings.data );
+                }
             }
-        }
-    });
+            if ( '/helpdesk/tickets/390/time_sheets/new' == settings.url ) {
+                displayFreeAgentProjects();
+                displayFreeAgentTasks();
+            }
+        } );
+    }
 };
+
+/**
+ * Run query to return projects and output html list on time entry form
+ */
+function displayFreeAgentProjects() {
+    var xhr = doRequest( 'GET', 'projects?view=active' );
+    xhr.setRequestHeader( 'Authorization', 'Bearer ' + freeagent_token );
+    xhr.setRequestHeader( 'Content-Type', 'application/json' );
+    xhr.onload = returnProjects;
+    xhr.send();
+}
+
+/**
+ * Run query to return tasks for current project and output html list on time entry form
+ */
+function displayFreeAgentTasks() {
+    var current_project = jQuery( 'select[name="freeagent_project"]' ).val();
+    var xhr             = doRequest( 'GET', 'tasks?project='+freeagent_url+'projects/'+current_project );
+    xhr.setRequestHeader( 'Authorization', 'Bearer ' + freeagent_token );
+    xhr.setRequestHeader( 'Content-Type', 'application/json' );
+    xhr.onload = returnTasks;
+    xhr.send();
+}
+
+/**
+ * Insert project list as select box on time entry form
+ */
+function returnProjects() {
+    projects = JSON.parse( this.responseText );
+    var html = '<dt><label for="freeagent_project">Project</label></dt>';
+    html    += '<dd id="freeagent_projects"><select name="freeagent_project" >';
+    if ( projects.projects.length > 0 ) {
+        jQuery.each( projects.projects, function ( key, project ) {
+            html += '<option value="' + getIDFromURL( project.url ) + '">' + project.name + '</option>';
+        } );
+    } else {
+        html += '<option value="">No projects available</option>';
+    }
+    html += '</select></dd>';
+    jQuery( html ).insertAfter( '#add_new_time_entry dl dd:first-of-type' );
+    addProjectChangeEvent();
+}
+
+/**
+ * Add event to change task list dependant on project chosen
+ */
+function addProjectChangeEvent() {
+    var select = jQuery( 'select[name="freeagent_project"]' );
+    select.on( 'change', function() {
+        console.log( 'changed!' );
+        refreshTasks( select.val() );
+    });
+}
+
+/**
+ * Insert task list as select box on time entry form
+ */
+function returnTasks() {
+    tasks = JSON.parse( this.responseText );
+    var html = '<dt class="freeagent_tasks"><label for="freeagent_task">Task</label></dt>';
+    html    += '<dd id="freeagent_tasks" class="freeagent_tasks"><select name="freeagent_task" >';
+    if ( tasks.tasks.length > 0 ) {
+        jQuery.each( tasks.tasks, function ( key, task ) {
+            html += '<option value="' + getIDFromURL( task.url ) + '">' + task.name + '</option>';
+        } );
+    } else {
+        html += '<option value="">No tasks available for current project</option>';
+    }
+    html += '</select></dd>';
+    jQuery( html ).insertAfter( '#add_new_time_entry dl dd#freeagent_projects' );
+}
+
+/**
+ * Remove task list and replace once new query has been run for chosen project
+ */
+function refreshTasks() {
+    jQuery( '.freeagent_tasks' ).remove();
+    displayFreeAgentTasks();
+}
+
+/**
+ * Return item ID from given URL
+ *
+ * @param url
+ * @returns {*|T}
+ */
+function getIDFromURL( url ) {
+    var parts = url.split( '/' );
+    return parts.pop();
+}
 
 /**
  * Create timeslip in freeagent
